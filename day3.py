c6036c1aca908b3e67eaf2628450c9f95093bc19
kyleaskine <61367480+kyleaskine@users.noreply.github.com>
2022-12-09 23:19:22 -0500

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,1 @@
+with open('day3.txt') as f:	sum = 0;	while True:		line = f.readline()		if not line:			print(sum)			break		array = list(line)		firstHalf = array[0:len(array) // 2]		secondHalf = array[len(array) // 2: len(array)]		for c in firstHalf:			if c in secondHalf:				intVal = ord(c)				if intVal < 96:					sum += intVal - 38				else:					sum += intVal - 96				break		
